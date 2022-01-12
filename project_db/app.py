ec4e3888e1a5e27fa8e87acc4ccd0fc1bf1f2bde
ThomasReichel <computer@web4tom.de>
2022-01-12 15:17:16 +0100

    post bsp

--- project_db/app.py
+++ project_db/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sqlite3
+
 
 app = Flask(__name__)
 
@@ -24,11 +26,33 @@ def neu():
 @app.route('/anzeigen', methods=['GET', 'POST'])
 def anzeigen():
     if request.method == 'POST':
-        name1 = request.form['name1']
-        name2 = request.form['name2']
-        name3 = request.form['name3']
-        return render_template("anzeigen.html", text1=name1, text2=name2, text3=name3)
+        bezeichnung = request.form['name1']
+        notiz = request.form['name2']
+        anzahl = request.form['name3']
 
+        verbindung = sqlite3.connect("database.db")
+        cursor = verbindung.cursor()
+
+        cursor.execute("""
+                        INSERT INTO artikel 
+                               VALUES (?,?,?)
+                       """,
+                       (bezeichnung, notiz, anzahl)
+                       )
+
+        verbindung.commit()
+        verbindung.close()
+        return render_template("anzeigen.html")
+
+
+@app.route('/auslesen', methods=['GET', 'POST'])
+def auslesen():
+    verbindung = sqlite3.connect("database.db")
+    zeiger = verbindung.cursor()
+    zeiger.execute("SELECT bezeichnung, notiz, anzahl FROM artikel")
+    inhalt = zeiger.fetchall()
+    verbindung.close()
+    return render_template("auslesen.html", bezeichnung=inhalt[0][0], notiz=inhalt[0][1], anzahl=inhalt[0][2])
 
 if __name__ == '__main__':
     app.run()
