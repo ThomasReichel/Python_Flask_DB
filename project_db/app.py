50a0d2bfbc212e1921df23b2662d0833c4ea26e5
ThomasReichel <computer@web4tom.de>
2022-01-24 20:15:51 +0100

    2022-1-24_20:15_login

--- project_db/app.py
+++ project_db/app.py
@@ -1,40 +1,72 @@
+'''
+https://github.com/arpanneupane19/Chattr/blob/main/app/main.py
+'''
+
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from flask_wtf import form, FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.validators import InputRequired, Length, ValidationError, DataRequired
 
-
+'''
+    Init der app und db
+'''
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'secretkey'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-
+'''
+    Tabelle für die User
+'''
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
-
+'''
+    Formular für die Registrierung
+'''
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    username = StringField(validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Register")
 
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:      # gibt den user schon
+            raise ValidationError(
+                "That username already exists. Please choose a different one.")
+
+'''
+    Formular für das Login
+'''
+class LoginForm(FlaskForm):
+    username = StringField(validators=[DataRequired(), Length(
+        min=4, max=15)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[DataRequired(), Length(
+        min=4, message="Password must be a minimum of %(min)d characters.")], render_kw={"placeholder": "Password (4 minimum)"})
+    submit = SubmitField("Login")
+
+
+'''
+    Routen definieren
+'''
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
